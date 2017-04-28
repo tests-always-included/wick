90fd2232c158bad8c9a0f272f2bee8c215de36b6
Tyler Akins <fidian@rumkin.com>
2017-04-28 09:14:37 -0500

    Fixes for BSD sed
    Already reviewed by Robin

--- util/tomdoc.sh
+++ util/tomdoc.sh
@@ -64,10 +64,36 @@ done
 
 # Regular expression matching whitespace.
 SPACE_RE='[[:space:]]*'
+
 # The inverse of the above, must match at least one character
 NOT_SPACE_RE='[^[:space:]][^[:space:]]*'
-# Regular expression matching shell function or variable name.
-NAME_RE='[a-zA-Z_][a-zA-Z0-9_]*'
+
+# Regular expression matching shell function or variable name.  Functions may
+# use nearly every character.  See [issue #8].  Disallowed characters (hex,
+# octal, then a description or a character):
+#
+#   00 000 null       01 001 SOH        09 011 Tab        0a 012 Newline
+#   20 040 Space      22 042 Quote      23 043 #          24 044 $
+#   26 046 &          27 047 Apostrophe 28 050 (          29 051 )
+#   2d 055 Hyphen     3b 073 ;          3c 074 <          3d 075 =
+#   3e 076 >          5b 133 [          5c 134 Backslash  60 140 Backtick
+#   7c 174 |          7f 177 Delete
+#
+# Exceptions allowed as leading character:  \x3d and \x5b
+# Exceptions allowed as secondary character: \x23 and \x2d
+#
+# Must translate to raw characters because Mac OS X's sed does not work with
+# escape sequences.  All escapes are handled by printf.
+#
+# [issue #8]: https://github.com/tests-always-included/tomdoc.sh/issues/8
+FUNC_NAME_RE=$(printf "[^\\001\\011 \"#$&'()\\055;<>\\134\\140|\\177][^\\001\\011 \"$&'();<=>[\\134\\140|\\177]*")
+
+# Regular expression matching variable names.  Similar to FUNC_NAME_RE.
+# Variables are far more restrictive.
+#
+# Leading characters can be A-Z, _, a-z.
+# Secondary characters can be 0-9, =, A-Z, _, a-z
+VAR_NAME_RE='[A-Z_a-z][0-9=A-Z_a-z]*'
 
 # Strip leading whitespace and '#' from TomDoc strings.
 #
@@ -121,8 +147,9 @@ generate_markdown() {
             if printf "%s" "$line" | grep -q "^$NOT_SPACE_RE"; then
                 printf "%s" "* $line"
             else
+                # Careful - BSD sed always adds a newline
                 printf "    * "
-                printf "%s" "$line" | sed "s/^$SPACE_RE//"
+                printf "%s" "$line" | sed "s/^$SPACE_RE//" | tr -d "\n"
             fi
 
             last_was_option=true
@@ -146,7 +173,8 @@ generate_markdown() {
                 "  "*)
                     # Examples and option continuation
                     if $last_was_option; then
-                        printf "%s" "$line" | sed "s/^ */ /"
+                        # Careful - BSD sed always adds a newline
+                        printf "%s" "$line" | sed "s/^ */ /" | tr -d "\n"
                         did_newline=false
                     else
                         printf "  %s\n" "$line"
@@ -196,10 +224,10 @@ generate_markdown() {
 # Returns nothing.
 parse_code() {
     sed -n \
-        -e "s/^$SPACE_RE\(function\)\{0,1\}$SPACE_RE\($NAME_RE\)$SPACE_RE().*$/\2()/p" \
-        -e "s/^$SPACE_RE\(export\)\{0,1\}$SPACE_RE\($NAME_RE\)=.*$/\2/p" \
-        -e "s/^${SPACE_RE}export$SPACE_RE\($NAME_RE\).*$/\1/p" \
-        -e "s/^$SPACE_RE:$SPACE_RE\${\($NAME_RE\):\{0,1\}=.*$/\1/p"
+        -e "s/^$SPACE_RE\(function\)\{0,1\}$SPACE_RE\($FUNC_NAME_RE\)$SPACE_RE().*$/\2()/p" \
+        -e "s/^$SPACE_RE\(export\)\{0,1\}$SPACE_RE\($VAR_NAME_RE\)=.*$/\2/p" \
+        -e "s/^${SPACE_RE}export$SPACE_RE\($VAR_NAME_RE\).*$/\1/p" \
+        -e "s/^$SPACE_RE:$SPACE_RE\${\($VAR_NAME_RE\):\{0,1\}=.*$/\1/p"
 }
 
 # Read lines from stdin, look for TomDoc'd shell functions and variables, and
